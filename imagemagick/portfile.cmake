2d778a4394760900793daa5c945ba07f876c9020
Schäfer <ru2063@partner.kit.edu>
2026-09-22 13:34:49 +0200

    Initial commit

---
+++ imagemagick/portfile.cmake
@@ -0,0 +1,46 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO ImageMagick/ImageMagick
+    REF 7.1.2-18
+    SHA512 168326e62c2ca5608a660e030b9777c7200458f9eb128854867e9bf20e1aa3d603a819aa3095fb3cf13133ceb9a9c9859374b7ce872bebb179c3afdf943ec831
+    HEAD_REF main
+    PATCHES
+        0001-fix-autotools-prefix-on-type-fallbacks.patch
+        0002-fix-fallthrough-msvc.patch
+)
+
+set(IM_CONFIGURE_ARGS "")
+list(APPEND IM_CONFIGURE_ARGS
+    "--disable-docs"
+    "--without-utilities"
+    "--without-perl"
+    "--disable-installed"
+)
+
+vcpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}"
+    USE_WRAPPERS
+    OPTIONS
+        ${IM_CONFIGURE_ARGS}
+)
+
+#fixing windows problems
+if(VCPKG_TARGET_IS_WINDOWS)
+    foreach(_config "rel" "dbg")
+        set(_makefile "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-${_config}/Makefile")
+        if(EXISTS "${_makefile}")
+            file(APPEND "${_makefile}"
+                "\n# emf.c genuinely needs C++ mode (uses the GDI+ C++ API)\n"
+                "coders/MagickCore_libMagickCore_7_Q16HDRI_la-emf.lo: CFLAGS += -TP\n"
+            )
+        endif()
+    endforeach()
+endif()
+
+vcpkg_build_make()
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
